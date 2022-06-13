7374cd7c8818df4010549d43c57e74f8c0e1afaf
diopp <49011476+diopp@users.noreply.github.com>
2022-06-13 12:40:33 -0700

    Currently working on the model version

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,5 @@
 import './Hero.css'
 import 'animate.css';
-import { Link } from "react-router-dom";
 
 
 function Hero () {
@@ -8,7 +7,7 @@ function Hero () {
         <div className=' hero border-l-4 '>
         <h1 id='h1'> Car Sharing Made Easy</h1>
         <p>Rent with style</p>
-        <a className=" button-1" href='https://www.hyrecar.com/howitworks' target="_blank" >
+        <a className=" button-1"  >
         Book Now
         </a>
 

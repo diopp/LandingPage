d96cef116df44d97b85bb7bf7f6ea539cbd563c7
diopp <49011476+diopp@users.noreply.github.com>
2022-06-20 11:48:23 -0700

    final product

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,7 +9,7 @@ function Hero () {
         <h1 id='h1'> Car Sharing Made Easy</h1>
         <p>Rent with style</p>
         <button className=" button-1"  >
-        <a href="https://www.hyrecar.com/" target="_blank" >Book Now</a>
+        <a href="https://www.hyrecar.com/" rel="noopener" target="_blank" >Book Now</a>
         </button>
 
         <button className='button-2'><a href="#one">About Us</a></button>
